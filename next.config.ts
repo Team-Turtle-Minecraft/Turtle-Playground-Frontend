22329c6a84f7c6a17c17a7999e2b9c71a5b2ca3e
Jeong Seonghun <2000tjdgns@naver.com>
2025-02-28 17:04:33 +0900

    gif 업로드

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // 정적 내보내기에 필요
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "3000",
+      },
+      {
+        protocol: "https",
+        hostname: "turtle-playground.kr",
+      },
+      {
+        protocol: "https",
+        hostname: "api.turtle-playground.kr",
       },
     ],
   },
